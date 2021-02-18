583b795331ef35f3d03b8fbf4c2814a616136680
InasseY <inasse.youssouf@gmail.com>
2021-02-18 09:55:22 +0100

    boucle while à retravailler

---
+++ Partie3_Ex3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Partie3_Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int result, number;
+            string reponseString ;
+            
+
+            Console.WriteLine("Pouvez vous renseigner le premier montant ?");
+            result = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("voulez vous y ajouter un chiffre oui/non");
+            reponseString = Console.ReadLine();
+
+            //tant que on répond oui on fait la boucle suivante 
+            while ( reponseString == "oui")
+            {  
+            Console.WriteLine($"quel est le chiffre à ajouter ?");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            result = result + number ;
+            Console.WriteLine($"le résultat est de {result}, voulez vous encore ajouter un chiffre oui/non");
+            reponseString = Console.ReadLine();
+
+
+        }
+                
+                {
+                    Console.WriteLine("Merci pour votre parcticipation");
+                }
+
+                
+            }
+        }
+    }
+
+
+ 
